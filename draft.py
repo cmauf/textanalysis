ebfca24dba1e2d292e0bd68bae5e2c3307cdc5aa
cmauf <constantin.mauf@outlook.de>
2020-01-08 23:34:20 +0100

    Added Sourcefile, reverted changes to matching
    We compiled a file that's the basis for our sentiment analysis.
    Changes to data representation of corpus reverted due to worse performance

--- draft.py
+++ draft.py
@@ -1,57 +1,59 @@
 #!/usr/bin/python3.8
-import re, string, sys
+import re, statistics, string, sys
 
-def setCorpus (): # erstellt den Korpus aus den bestehenden Dateien
-	sources = ['SentiWS_v2.0_Negative.txt', 'SentiWS_v2.0_Positive.txt']
+def setCorpus (): # erstellt den Korpus aus der mitgelieferten Datei
 	corpus = []
 	try: #Korpus-file existiert nicht
-		for file in sources:
-			source = open(file, encoding='utf-8')
-			buffer = source.read().split('\n')
-			for line in buffer:
-				#print(line)
-				if len(line) > 0:
-					temp = line.split('\t')
-					splitWordType = temp[0].split('|')
-					baseform = splitWordType[0]
-					type = splitWordType[1]
-					sentiment = temp[1]
-					variants = temp[2].split(',')
-					corpus.append(dict(word = baseform, type = type, sentiment = float(sentiment)))
-					for var in variants:
-						if var.isalpha():
-							corpus.append(dict(word = var, type = type, sentiment = float(sentiment)))
-						
-		#print(corpus)
+		source = open('sortedCorpus.txt', encoding='utf-8')
+		buffer = source.read().split('\n')
+		for line in buffer:
+			if len(line) > 0:
+				temp = line.split('\t')
+				splitWordType = temp[0].split('|')
+				baseform = splitWordType[0]
+				type = splitWordType[1]
+				sentiment = temp[1]
+				variants = temp[2].split(',')
+				corpus.append(dict(baseform = baseform, type = type, sentiment = float(sentiment), variants=variants))
+	
 		return corpus
 	except:
-		print('Die Corpus-Dateien sind nicht hinterlegt!')
+		print('Die Corpus-Datei ist nicht hinterlegt!')
 		quit()
 
 def incTextStuff (entry):
 	if entry.get('type') == 'VVINF':
 		global verbs
 		verbs = verbs + 1
-	if entry.get('type') == 'ADJX':
+	elif entry.get('type') == 'ADJX':
 		global adjectives
 		adjectives = adjectives + 1
-	if entry.get('type') == 'ADV':
+	elif entry.get('type') == 'ADV':
 		global adverbs
 		adverbs = adverbs + 1
-	if entry.get('type') == 'NN':
+	elif entry.get('type') == 'NN':
 		global nouns
 		nouns = nouns + 1
-	global sentiment
-	sentiment =+ entry.get('sentiment')
+	if entry.get('sentiment') < 0:
+		global sentimentNeg, negCount
+		sentimentNeg.append(entry.get('sentiment'))
+		negCount += 1
+	else:
+		global sentimentPos, posCount
+		sentimentPos.append(entry.get('sentiment'))
+		posCount += 1
 
 def verbalizeSentiment ():
-	global sentiment
-	verbalizedSentiment = ''
-	if sentiment > 0.2:
+	sentiment = getSentiment()
+	if sentiment > 0.3:
 		verbalizedSentiment = 'sehr positiv'
-	elif sentiment > 0:
+	elif sentiment > 0.1:
 		verbalizedSentiment = 'eher positiv'
-	elif sentiment < -0.2:
+	elif sentiment > 0:
+		verbalizedSentiment = 'leicht positiv'
+	elif sentiment < -0.1:
+		verbalizedSentiment = 'leicht negativ'
+	elif sentiment < -0.3:
 		verbalizedSentiment = 'sehr negativ'
 	elif sentiment < 0:
 		verbalizedSentiment = 'eher negativ'
@@ -72,18 +74,29 @@ def countWord (word):
 		else:
 			wordCounts[word] = 1
 
+def getSentiment():
+	global sentimentNeg, sentimentPos, posCount, negCount
+	allWords = posCount + negCount
+	pos = statistics.median(sentimentPos)
+	neg = statistics.median(sentimentNeg)
+	sentiment = float(posCount)/allWords * pos + float(negCount)/allWords * neg
+	sentiment = round(sentiment, 4)
+	#print(f'Es gibt {negCount} negative Woerter mit einem Score von {neg} und {posCount} positive Worter mit einem Score von {pos}. Gewichtet ergibt das einen Score von {sentiment}.')
+	return sentiment
+
 def checkSentiment (word):
 	for entry in corpus:
 		#if len(word) == 0 or not word.isalpha():
 			#continue
-		if word == entry.get('word'):
-			print(word)
+		if word == entry.get('baseform') or word in entry.get('variants'):
+			#print(word)
 			incTextStuff(entry)
 		else:
 			#print(word + ' wurde nicht im Korpus gefunden')
 			continue
 def printOutput():
 	if flags[5]['value'] == True:
+		sentiment = getSentiment()
 		output= 'Es gibt {} Woerter im Text, davon {} Adjektive, {} Adverbien, {} Verben und {} Substantive. Die Stimmung des Textes ist mit {} {}.\n'
 		print(output.format(wordCounter, adjectives, adverbs, verbs, nouns, sentiment, verbalizeSentiment()))
 	outputWords = int(len(wordCounts) / 10)
@@ -108,8 +121,7 @@ def setFlags():
 		 {'name': 'logging', 'short': '-l', 'value': False}, #flags[3]
 		 {'name': 'stopwords', 'short': '-o', 'value': False, 'source': ''}, #flags[4]
 		 {'name': 'sentiment', 'short': '-s', 'value': False}, #flags[5]
-		 {'name': 'prompt', 'short': '-p', 'value': False}, #flags[6]
-		 {'name': 'outputnumbers', 'short': '-n', 'value': False}] #flags[7]
+		 {'name': 'prompt', 'short': '-p', 'value': False}] #flags[6]
 	args = sys.argv
 	if len(args) == 1:
 		return flags
@@ -173,15 +185,19 @@ adjectives = 0
 adverbs = 0
 verbs = 0
 nouns = 0
-sentiment = 0.0
+sentimentPos = []
+sentimentNeg = []
+posCount = 0
+negCount = 0
 wordCounts = dict()
 for word in wordList:
 	#print(word)
 	wordCounter += 1
-	word = word.strip('.,-;:!\"§$%&/?()=\'')
+	word = word.strip('.,-;:!\"»«§$%&/?()=\'')
 	#print('analysiere ' + word)
 	if len(word) != 0:
 		countWord(word)
 		if flags[5]['value'] == True:
 			checkSentiment(word)
 printOutput()
+
